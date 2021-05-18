f233c28a89bf198cdbf55894b961c89f74db2ab5
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-18 22:52:45 +0300

    use static data within QuickUserToggler for now

--- src/_metronic/layout/components/extras/QuiclUserToggler.js
+++ src/_metronic/layout/components/extras/QuiclUserToggler.js
@@ -2,24 +2,24 @@
 /* eslint-disable no-script-url,jsx-a11y/anchor-is-valid */
 import React, { useMemo } from "react";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
-import { useSelector, shallowEqual } from "react-redux";
 import objectPath from "object-path";
 import { useHtmlClassService } from "../../_core/MetronicLayout";
 import { UserProfileDropdown } from "./dropdowns/UserProfileDropdown";
 
 export function QuickUserToggler() {
   const user = {
-    pic: 'https://st4.depositphotos.com/12982378/22072/i/600/depositphotos_220729084-stock-photo-smiling-adult-man-crossed-arms.jpg',
-    firstName: 'Mohammed',
-    lastName: 'Jaber',
-    occupation: 'React developer',
-    email: 'mohjbr.dev@gmail.com'
+    pic:
+      "https://st4.depositphotos.com/12982378/22072/i/600/depositphotos_220729084-stock-photo-smiling-adult-man-crossed-arms.jpg",
+    firstName: "Mohammed",
+    lastName: "Jaber",
+    occupation: "React developer",
+    email: "mohjbr.dev@gmail.com"
   };
   const uiService = useHtmlClassService();
   const layoutProps = useMemo(() => {
     return {
       offcanvas:
-        objectPath.get(uiService.config, "extras.user.layout") === "offcanvas",
+        objectPath.get(uiService.config, "extras.user.layout") === "offcanvas"
     };
   }, [uiService]);
 
