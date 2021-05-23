5ec7419b860b5603acfb183e9b77dfbba7c049db
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-23 16:32:47 +0300

    remove redux from CustomerEditDialog

--- src/app/modules/ECommerce/pages/customers/customer-edit-dialog/CustomerEditDialog.js
+++ src/app/modules/ECommerce/pages/customers/customer-edit-dialog/CustomerEditDialog.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useMemo } from "react";
 import { Modal } from "react-bootstrap";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import * as actions from "../../../_redux/customers/customersActions";
 import { CustomerEditDialogHeader } from "./CustomerEditDialogHeader";
 import { CustomerEditForm } from "./CustomerEditForm";
 import { useCustomersUIContext } from "../CustomersUIContext";
@@ -11,33 +9,27 @@ export function CustomerEditDialog({ id, show, onHide }) {
   const customersUIContext = useCustomersUIContext();
   const customersUIProps = useMemo(() => {
     return {
-      initCustomer: customersUIContext.initCustomer,
+      initCustomer: customersUIContext.initCustomer
     };
   }, [customersUIContext]);
 
   // Customers Redux state
-  const dispatch = useDispatch();
-  const { actionsLoading, customerForEdit } = useSelector(
-    (state) => ({
-      actionsLoading: state.customers.actionsLoading,
-      customerForEdit: state.customers.customerForEdit,
-    }),
-    shallowEqual
-  );
+  const actionsLoading = false;
+  const customerForEdit = 0;
 
   useEffect(() => {
     // server call for getting Customer by id
-    dispatch(actions.fetchCustomer(id));
-  }, [id, dispatch]);
+    // dispatch(actions.fetchCustomer(id));
+  }, [id]);
 
   // server request for saving customer
-  const saveCustomer = (customer) => {
+  const saveCustomer = customer => {
     if (!id) {
       // server request for creating customer
-      dispatch(actions.createCustomer(customer)).then(() => onHide());
+      // dispatch(actions.createCustomer(customer)).then(() => onHide());
     } else {
       // server request for updating customer
-      dispatch(actions.updateCustomer(customer)).then(() => onHide());
+      // dispatch(actions.updateCustomer(customer)).then(() => onHide());
     }
   };
 
