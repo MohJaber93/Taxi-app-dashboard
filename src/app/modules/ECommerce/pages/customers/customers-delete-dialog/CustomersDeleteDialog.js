2695655d94aad7b86201fd917b777786f8db21b2
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-25 23:19:07 +0300

    remove redux logic from CustomersDeleteDialog component

--- src/app/modules/ECommerce/pages/customers/customers-delete-dialog/CustomersDeleteDialog.js
+++ src/app/modules/ECommerce/pages/customers/customers-delete-dialog/CustomersDeleteDialog.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useMemo } from "react";
 import { Modal } from "react-bootstrap";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import * as actions from "../../../_redux/customers/customersActions";
 import { useCustomersUIContext } from "../CustomersUIContext";
-import {ModalProgressBar} from "../../../../../../_metronic/_partials/controls";
+import { ModalProgressBar } from "../../../../../../_metronic/_partials/controls";
 
 export function CustomersDeleteDialog({ show, onHide }) {
   // Customers UI Context
@@ -12,16 +11,12 @@ export function CustomersDeleteDialog({ show, onHide }) {
     return {
       ids: customersUIContext.ids,
       setIds: customersUIContext.setIds,
-      queryParams: customersUIContext.queryParams,
+      queryParams: customersUIContext.queryParams
     };
   }, [customersUIContext]);
 
   // Customers Redux state
-  const dispatch = useDispatch();
-  const { isLoading } = useSelector(
-    (state) => ({ isLoading: state.customers.actionsLoading }),
-    shallowEqual
-  );
+  const isLoading = false;
 
   // if customers weren't selected we should close modal
   useEffect(() => {
@@ -32,21 +27,21 @@ export function CustomersDeleteDialog({ show, onHide }) {
   }, [customersUIProps.ids]);
 
   // looking for loading/dispatch
-  useEffect(() => {}, [isLoading, dispatch]);
+  useEffect(() => {}, [isLoading]);
 
   const deleteCustomers = () => {
     // server request for deleting customer by selected ids
-    dispatch(actions.deleteCustomers(customersUIProps.ids)).then(() => {
-      // refresh list after deletion
-      dispatch(actions.fetchCustomers(customersUIProps.queryParams)).then(
-        () => {
-          // clear selections list
-          customersUIProps.setIds([]);
-          // closing delete modal
-          onHide();
-        }
-      );
-    });
+    // dispatch(actions.deleteCustomers(customersUIProps.ids)).then(() => {
+    //   // refresh list after deletion
+    //   dispatch(actions.fetchCustomers(customersUIProps.queryParams)).then(
+    //     () => {
+    //       // clear selections list
+    //       customersUIProps.setIds([]);
+    //       // closing delete modal
+    //       onHide();
+    //     }
+    //   );
+    // });
   };
 
   return (
