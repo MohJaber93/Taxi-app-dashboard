fd04fc353a8f26c5a2b75435ff1fe8385c1a2837
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-26 23:06:28 +0300

    remove redux logic from CustomersLoadingDialog

--- src/app/modules/ECommerce/pages/customers/customers-loading-dialog/CustomersLoadingDialog.js
+++ src/app/modules/ECommerce/pages/customers/customers-loading-dialog/CustomersLoadingDialog.js
@@ -1,13 +1,9 @@
 import React, { useEffect } from "react";
-import { shallowEqual, useSelector } from "react-redux";
 import { LoadingDialog } from "../../../../../../_metronic/_partials/controls";
 
 export function CustomersLoadingDialog() {
   // Customers Redux state
-  const { isLoading } = useSelector(
-    (state) => ({ isLoading: state.customers.listLoading }),
-    shallowEqual
-  );
+  const { isLoading } = false;
   // looking for loading/dispatch
   useEffect(() => {}, [isLoading]);
   return <LoadingDialog isLoading={isLoading} text="Loading ..." />;
