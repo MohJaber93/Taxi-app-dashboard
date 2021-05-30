ab7c8b53d39111eec2ef9a8de4e8fae6aba23215
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-05-30 15:02:23 +0300

    remove redux from ProductDeleteDialog

--- src/app/modules/ECommerce/pages/products/product-delete-dialog/ProductDeleteDialog.js
+++ src/app/modules/ECommerce/pages/products/product-delete-dialog/ProductDeleteDialog.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-restricted-imports */
 import React, { useEffect, useMemo } from "react";
 import { Modal } from "react-bootstrap";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { ModalProgressBar } from "../../../../../../_metronic/_partials/controls";
 import * as actions from "../../../_redux/products/productsActions";
 import { useProductsUIContext } from "../ProductsUIContext";
@@ -12,16 +11,12 @@ export function ProductDeleteDialog({ id, show, onHide }) {
   const productsUIProps = useMemo(() => {
     return {
       setIds: productsUIContext.setIds,
-      queryParams: productsUIContext.queryParams,
+      queryParams: productsUIContext.queryParams
     };
   }, [productsUIContext]);
 
   // Products Redux state
-  const dispatch = useDispatch();
-  const { isLoading } = useSelector(
-    (state) => ({ isLoading: state.products.actionsLoading }),
-    shallowEqual
-  );
+  const isLoading = false;
 
   // if !id we should close modal
   useEffect(() => {
@@ -32,18 +27,18 @@ export function ProductDeleteDialog({ id, show, onHide }) {
   }, [id]);
 
   // looking for loading/dispatch
-  useEffect(() => {}, [isLoading, dispatch]);
+  useEffect(() => {}, [isLoading]);
 
   const deleteProduct = () => {
     // server request for deleting product by id
-    dispatch(actions.deleteProduct(id)).then(() => {
-      // refresh list after deletion
-      dispatch(actions.fetchProducts(productsUIProps.queryParams));
-      // clear selections list
-      productsUIProps.setIds([]);
-      // closing delete modal
-      onHide();
-    });
+    // dispatch(actions.deleteProduct(id)).then(() => {
+    //   // refresh list after deletion
+    //   dispatch(actions.fetchProducts(productsUIProps.queryParams));
+    //   // clear selections list
+    //   productsUIProps.setIds([]);
+    //   // closing delete modal
+    //   onHide();
+    // });
   };
 
   return (
