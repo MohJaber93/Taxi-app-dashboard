20cbff0efae987b30d293e7371d60d00370a9a3f
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-06-07 12:32:42 +0300

    remove redux from RemarkEditDialog

--- src/app/modules/ECommerce/pages/products/product-remarks/remark-edit-dialog/RemarkEditDialog.js
+++ src/app/modules/ECommerce/pages/products/product-remarks/remark-edit-dialog/RemarkEditDialog.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo } from "react";
 import { Modal } from "react-bootstrap";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import * as actions from "../../../../_redux/remarks/remarksActions";
 import { RemarkEditDialogHeader } from "./RemarkEditDialogHeader";
 import { RemarkEditForm } from "./RemarkEditForm";
@@ -33,60 +32,54 @@ export function RemarkEditDialog() {
       queryParams: remarksUIContext.queryParams,
       showEditRemarkDialog: remarksUIContext.showEditRemarkDialog,
       closeEditRemarkDialog: remarksUIContext.closeEditRemarkDialog,
-      initRemark: remarksUIContext.initRemark,
+      initRemark: remarksUIContext.initRemark
     };
   }, [remarksUIContext]);
 
   // Remarks Redux state
-  const dispatch = useDispatch();
-  const { actionsLoading, remarkForEdit } = useSelector(
-    (state) => ({
-      actionsLoading: state.remarks.actionsLoading,
-      remarkForEdit: state.remarks.remarkForEdit,
-    }),
-    shallowEqual
-  );
+  const actionsLoading = false;
+  const remarkForEdit = null;
 
   useEffect(() => {
     // server request for getting remark by seleted id
-    dispatch(actions.fetchRemark(remarksUIProps.id));
-  }, [remarksUIProps.id, dispatch]);
+    // dispatch(actions.fetchRemark(remarksUIProps.id));
+  }, [remarksUIProps.id]);
 
-  const saveRemark = (remark) => {
+  const saveRemark = remark => {
     remark.dueDate = getFormattedDate(remark.dueDate);
     if (!remarksUIProps.id) {
       // server request for creating remarks
-      dispatch(actions.createRemark(remark)).then(() => {
-        // refresh list after deletion
-        dispatch(
-          actions.fetchRemarks(
-            remarksUIProps.queryParams,
-            remarksUIProps.productId
-          )
-        ).then(() => {
-          // clear selections list
-          remarksUIProps.setIds([]);
-          // closing edit modal
-          remarksUIProps.closeEditRemarkDialog();
-        });
-      });
+      // dispatch(actions.createRemark(remark)).then(() => {
+      //   // refresh list after deletion
+      //   dispatch(
+      //     actions.fetchRemarks(
+      //       remarksUIProps.queryParams,
+      //       remarksUIProps.productId
+      //     )
+      //   ).then(() => {
+      //     // clear selections list
+      //     remarksUIProps.setIds([]);
+      //     // closing edit modal
+      //     remarksUIProps.closeEditRemarkDialog();
+      //   });
+      // });
     } else {
       // server request for updating remarks
-      dispatch(actions.updateRemark(remark)).then(() => {
-        // refresh list after deletion
-        dispatch(
-          // refresh list after deletion
-          actions.fetchRemarks(
-            remarksUIProps.queryParams,
-            remarksUIProps.productId
-          )
-        ).then(() => {
-          // clear selections list
-          remarksUIProps.setIds([]);
-          // closing edit modal
-          remarksUIProps.closeEditRemarkDialog();
-        });
-      });
+      // dispatch(actions.updateRemark(remark)).then(() => {
+      //   // refresh list after deletion
+      //   dispatch(
+      //     // refresh list after deletion
+      //     actions.fetchRemarks(
+      //       remarksUIProps.queryParams,
+      //       remarksUIProps.productId
+      //     )
+      //   ).then(() => {
+      //     // clear selections list
+      //     remarksUIProps.setIds([]);
+      //     // closing edit modal
+      //     remarksUIProps.closeEditRemarkDialog();
+      //   });
+      // });
     }
   };
 
