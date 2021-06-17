157cb4bfe8622dcb29f2fda66c45d1160d1f5e85
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-06-17 22:16:11 +0300

    remove redux selector from SpecificationsLoadingDialog

--- src/app/modules/ECommerce/pages/products/product-specifications/SpecificationsLoadingDialog.js
+++ src/app/modules/ECommerce/pages/products/product-specifications/SpecificationsLoadingDialog.js
@@ -1,12 +1,8 @@
 import React, { useEffect } from "react";
-import { shallowEqual, useSelector } from "react-redux";
 import { LoadingDialog } from "../../../../../../_metronic/_partials/controls";
 
 export function SpecificationsLoadingDialog() {
-  const { isLoading } = useSelector(
-    (state) => ({ isLoading: state.specifications.listLoading }),
-    shallowEqual
-  );
+  const isLoading = false;
   useEffect(() => {}, [isLoading]);
   return <LoadingDialog isLoading={isLoading} text="Loading ..." />;
 }
