7862bf09f880d8cd2e9b2e2563046b37273673ce
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-06-20 22:23:21 +0300

    remove redux logic from SpecificationEditDialog

--- src/app/modules/ECommerce/pages/products/product-specifications/specification-edit-dialog/SpecificationEditDialog.js
+++ src/app/modules/ECommerce/pages/products/product-specifications/specification-edit-dialog/SpecificationEditDialog.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo } from "react";
 import { Modal } from "react-bootstrap";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import * as actions from "../../../../_redux/specifications/specificationsActions";
 import { SpecificationEditDialogHeader } from "./SpecificationEditDialogHeader";
 import { SpecificationEditForm } from "./SpecificationEditForm";
@@ -16,44 +15,38 @@ export function SpecificationEditDialog() {
       onHide: specsUIContext.closeEditSpecificationDialog,
       productId: specsUIContext.productId,
       queryParams: specsUIContext.queryParams,
-      initSpecification: specsUIContext.initSpecification,
+      initSpecification: specsUIContext.initSpecification
     };
   }, [specsUIContext]);
 
   // Specifications Redux state
-  const dispatch = useDispatch();
-  const { actionsLoading, specificationForEdit } = useSelector(
-    (state) => ({
-      actionsLoading: state.specifications.actionsLoading,
-      specificationForEdit: state.specifications.specificationForEdit,
-    }),
-    shallowEqual
-  );
+  const actionsLoading = false;
+  const specificationForEdit = null;
 
   useEffect(() => {
     // server request for getting spec by seleted id
-    dispatch(actions.fetchSpecification(specsUIProps.id));
-  }, [specsUIProps.id, dispatch]);
+    // dispatch(actions.fetchSpecification(specsUIProps.id));
+  }, [specsUIProps.id]);
 
-  const saveSpecification = (specification) => {
+  const saveSpecification = specification => {
     if (!specsUIProps.id) {
-      dispatch(actions.createSpecification(specification)).then(() => {
-        dispatch(
-          actions.fetchSpecifications(
-            specsUIProps.queryParams,
-            specsUIProps.productId
-          )
-        ).then(() => specsUIProps.onHide());
-      });
+      // dispatch(actions.createSpecification(specification)).then(() => {
+      //   dispatch(
+      //     actions.fetchSpecifications(
+      //       specsUIProps.queryParams,
+      //       specsUIProps.productId
+      //     )
+      //   ).then(() => specsUIProps.onHide());
+      // });
     } else {
-      dispatch(actions.updateSpecification(specification)).then(() => {
-        dispatch(
-          actions.fetchSpecifications(
-            specsUIProps.queryParams,
-            specsUIProps.productId
-          )
-        ).then(() => specsUIProps.onHide());
-      });
+      // dispatch(actions.updateSpecification(specification)).then(() => {
+      //   dispatch(
+      //     actions.fetchSpecifications(
+      //       specsUIProps.queryParams,
+      //       specsUIProps.productId
+      //     )
+      //   ).then(() => specsUIProps.onHide());
+      // });
     }
   };
 
