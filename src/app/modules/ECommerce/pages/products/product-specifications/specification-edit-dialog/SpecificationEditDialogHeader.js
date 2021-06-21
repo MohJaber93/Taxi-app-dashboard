2de6993fa669c5bb0db18a604ffb3eb315bed595
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-06-21 20:40:27 +0300

    remove redux selector from SpecificationEditDialogHeader

--- src/app/modules/ECommerce/pages/products/product-specifications/specification-edit-dialog/SpecificationEditDialogHeader.js
+++ src/app/modules/ECommerce/pages/products/product-specifications/specification-edit-dialog/SpecificationEditDialogHeader.js
@@ -1,19 +1,13 @@
 /* eslint-disable no-restricted-imports */
 import React, { useState, useEffect } from "react";
-import { shallowEqual, useSelector } from "react-redux";
 import { Modal } from "react-bootstrap";
 import { ModalProgressBar } from "../../../../../../../_metronic/_partials/controls";
 
 export function SpecificationEditDialogHeader({ id }) {
   const [title, setTitle] = useState("");
   // Specs Redux state
-  const { specificationForEdit, actionsLoading } = useSelector(
-    (state) => ({
-      specificationForEdit: state.specifications.specificationForEdit,
-      actionsLoading: state.specifications.actionsLoading,
-    }),
-    shallowEqual
-  );
+  const actionsLoading = false;
+  const specificationForEdit = null;
 
   useEffect(() => {
     let _title = id ? "" : "New Specification";
