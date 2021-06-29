31589a60835783aa6754c0e4faa3d5021d765871
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-06-29 11:57:18 +0300

    remove resux selector ProductsLoadingDialog

--- src/app/modules/ECommerce/pages/products/products-loading-dialog/ProductsLoadingDialog.js
+++ src/app/modules/ECommerce/pages/products/products-loading-dialog/ProductsLoadingDialog.js
@@ -1,12 +1,8 @@
-import React, {useEffect} from "react";
-import {shallowEqual, useSelector} from "react-redux";
-import {LoadingDialog} from "../../../../../../_metronic/_partials/controls";
+import React, { useEffect } from "react";
+import { LoadingDialog } from "../../../../../../_metronic/_partials/controls";
 
 export function ProductsLoadingDialog() {
-  const { isLoading } = useSelector(
-    state => ({ isLoading: state.products.listLoading }),
-    shallowEqual
-  );
+  const isLoading = false;
   useEffect(() => {}, [isLoading]);
   return <LoadingDialog isLoading={isLoading} text="Loading ..." />;
 }
